d4fd882ab49c5e9b30be6377534f35da94951640
Chris Troutner <chris.troutner@gmail.com>
2017-06-26 09:04:56 -0700

    Customizing the plugin

--- keystone/routes/recaptchaRouter.js
+++ keystone/routes/recaptchaRouter.js
@@ -14,10 +14,10 @@ module.exports = function(app) {
   //app.get('/test', routes.views.test);
   
   // Plugin API Route
-  app.get('/api/recaptchaplugin/list', keystone.middleware.api, routes.api.exampleplugin.list);
-  app.all('/api/recaptchaplugin/create', keystone.middleware.api, routes.api.exampleplugin.create);
-  app.all('/api/recaptchaplugin/:id/update', keystone.middleware.api, routes.api.exampleplugin.update);
-	app.get('/api/recaptchaplugin/:id/remove', keystone.middleware.api, routes.api.exampleplugin.remove);
+  app.get('/api/recaptchaplugin/list', keystone.middleware.api, routes.api.recaptchaplugin.list);
+  app.all('/api/recaptchaplugin/create', keystone.middleware.api, routes.api.recaptchaplugin.create);
+  app.all('/api/recaptchaplugin/:id/update', keystone.middleware.api, routes.api.recaptchaplugin.update);
+	app.get('/api/recaptchaplugin/:id/remove', keystone.middleware.api, routes.api.recaptchaplugin.remove);
   
   // NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
